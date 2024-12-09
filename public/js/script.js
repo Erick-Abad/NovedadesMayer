4e64f9ef27103c3d85e4829eb597a8d7d09cca2e
mreyes96 <reyesmayra1506@gmail.com>
2024-12-08 23:00:29 -0500

    Subir todas las carpetas y archivos del proyecto

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-menu');
+  
+    hamburger.addEventListener('click', () => {
+      mobileMenu.classList.toggle('active');
+    });
+  });
+  
